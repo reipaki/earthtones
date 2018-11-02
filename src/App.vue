1e15a3c470596d2f34f3b32a0812e7d4107ef7ba
reipaki <rei@salience.com.au>
2018-11-03 00:50:53 +1100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app dark>
+
+    <v-content style="background: url(art.png); background-size: cover; background-repeat: no-repeat; background-position: center center;">
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+
+      <v-flex xs12 sm8 offset-sm2 my-5 pa-4 style="background:rgba(0,0,0,0.6);">
+
+        <h1 class="hidden-xs-only display-2 mt-3 mb-3">
+          EARTHTONES
+        </h1>
+        <h1 class="visible-xs-only display-1 mt-3 mb-3">
+          EARTHTONES
+        </h1>
+        <h2 class="font-weight-bold mb-4 mt-4">
+          Interior Designer | Artist | Photographer
+        </h2>
+        <p class="subheading font-weight-regular">
+          <b>Earthtones</b> is the online home of creative, <b>Annette Paki</b>.
+        </p>
+        <p class="subheading font-weight-regular">
+          With a background in artistic expression, she has now focused her attention toward interior design – where her instinctive skills in form, colour, light, texture and space find a broader canvas on which to work.
+        </p>
+        <p class="subheading font-weight-regular">
+          With an artistic style that approaches abstract expressionism and head for connecting challenging forms with everyday function, Annette is as comfortable with brush and palette knife as she is with visualising three-dimensional space.
+        </p>
+        <!-- <p class="subheading font-weight-regular">
+          Raw texture light the importance of a connection to nature assymetry raw expressionh
+        </p> -->
+        <p class="subheading font-weight-regular">
+          Her collection of artwork and photography will be back online soon, along with interior design projects showcasing her approach, concepts and capabilities.
+        </p>
+        <hr>
+        <p class="subheading font-weight-regular mt-4">
+          Annette is currently studying Interior Design and Decoration at the Design Centre, Enmore in Sydney, Australia. For enquiries, please contact her directly via email at: <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#97;&#110;&#110;&#101;&#116;&#116;&#101;&#64;&#101;&#97;&#114;&#116;&#104;&#116;&#111;&#110;&#101;&#115;&#46;&#110;&#101;&#116;&#46;&#97;&#117;" class="white--text font-weight-bold">&#97;&#110;&#110;&#101;&#116;&#116;&#101;&#64;&#101;&#97;&#114;&#116;&#104;&#116;&#111;&#110;&#101;&#115;&#46;&#110;&#101;&#116;&#46;&#97;&#117;</a>.
+        </p>
+      </v-flex>
+
+
+    </v-layout>
+  </v-container>
+</template>
+
+
+
+<style>
+  h1.display-2 {
+    font-family: "Barlow";
+    font-size: 60px;
+  }
+  h2 {
+    font-family: "Martel";
+    font-weight: 100;
+  }
+  p {
+    font-family: "Martel-Light";
+    font-weight: lighter;
+  }
+  hr {
+    background-color: grey;
+    border-color: grey;
+  }
+
+</style>
